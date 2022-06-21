f8ac89c2d19ee7f3d9944f99a457f6702529cbb6
Alihassan20 <43120067+Alihassan20@users.noreply.github.com>
2022-06-21 18:08:17 +0400

    Initial commit

---
+++ lib/core/database/datab.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class SqlDb{
+
+  intialDb() async{
+    var databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'ali.db');
+    var myDb = await openDatabase(path, onCreate:_onCreate);
+    return myDb;
+  }
+  _onCreate(Database db, int version)async{
+    await db.execute(
+        'CREATE TABLE notes (id INTEGER PRIMARY KEY, name TEXT, value INTEGER, num REAL)');
+  }
+}
