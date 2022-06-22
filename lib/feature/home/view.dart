53c0d4c0b6b6f162f43da21bcda2800873cb454f
Alihassan20 <43120067+Alihassan20@users.noreply.github.com>
2022-06-22 17:08:45 +0400

    12

--- lib/feature/home/view.dart
+++ lib/feature/home/view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:sql_demo/core/database/datab.dart';
 import 'package:sql_demo/feature/addNote/view.dart';
 
@@ -21,15 +22,15 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(" Your Notes "),
+        title: const Text(" Your Notes "),
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context)
-              .push(MaterialPageRoute(builder: (_) => AddNote()));
+              .push(MaterialPageRoute(builder: (_) => const AddNote()));
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
       body: ListView(
         children: [
@@ -38,7 +39,7 @@ class _HomeState extends State<Home> {
                 await sqlDb.deleteDatabaseDone();
                 print("done");
               },
-              child: Text("DELETE ALL DATABASE ")),
+              child: const Text("DELETE ALL DATABASE ")),
           FutureBuilder(
               future: readData(),
               builder:
@@ -49,14 +50,125 @@ class _HomeState extends State<Home> {
                       shrinkWrap: true,
                       physics: const NeverScrollableScrollPhysics(),
                       itemBuilder: (context, index) {
-                        return Card(
-                            child: ListTile(
-                          title: Text("${snapshot.data![index]['title']}"),
-                          trailing: Text("${snapshot.data![index]['note']}"),
-                        ));
+                        return Dismissible(
+                          key: Key('item ${snapshot.data![index]['id']}'),
+                          onDismissed: (DismissDirection direction) {
+                            setState(() {
+                              snapshot.data!.removeAt(index);
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: const Text("deleted"),
+                                action: SnackBarAction(
+                                  label: "Undo",
+                                  onPressed: () {
+                                    setState(() {
+                                      snapshot.data!.insert(
+                                          index, snapshot.data![index]);
+                                    });
+                                  },
+                                ),
+                              ));
+                            });
+                          },
+                          confirmDismiss: (DismissDirection direction) async {
+                            await showDialog(
+                                context: context,
+                                builder: (_) {
+                                  return AlertDialog(
+                                    content: Text(
+                                        "Are You Sure You Want To Delete This ${snapshot.data![index]['note']}"),
+                                    actions: [
+                                      ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                              shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          15)),
+                                              primary: Colors.transparent,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20,
+                                                      vertical: 10),
+                                              textStyle: const TextStyle(
+                                                  fontSize: 25,
+                                                  fontWeight: FontWeight.bold)),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: const Text("Cancel")),
+                                      ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                              shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          15)),
+                                              primary: const Color.fromRGBO(
+                                                  255, 91, 56, 1),
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20,
+                                                      vertical: 10),
+                                              textStyle: const TextStyle(
+                                                  fontSize: 25,
+                                                  fontWeight: FontWeight.bold)),
+                                          onPressed: () async{
+                                            setState(() {
+                                                sqlDb.deleteData("DELETE FROM 'notes' WHERE id = ${snapshot.data![index]['id']}");
+                                              snapshot.data!.remove(snapshot.data![index]);
+                                              print("delet+++++++++++++++");
+                                                Get.snackbar(
+                                                  'Your Note has been updated',
+                                                  'Do you want to Undo?',
+                                                  mainButton: TextButton(
+                                                      onPressed: () {
+                                                        setState(() {
+                                                          snapshot.data!.insert(
+                                                              index, snapshot.data![index]);
+                                                        });
+                                                      },
+                                                      child: const Text("Undo")),
+                                                );
+                                                Navigator.of(context).pop();
+                                            });
+                                          },
+                                          child: const Text("Delete")),
+                                    ],
+                                  );
+                                });
+                          },
+                          background: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(5)),
+                            alignment: Alignment.center,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  'Delete',
+                                  style: TextStyle(color: Colors.redAccent),
+                                ),
+                                Icon(
+                                  Icons.delete_forever,
+                                  color: Colors.redAccent,
+                                  size: 40,
+                                ),
+                              ],
+                            ),
+                          ),
+                          child: GestureDetector(
+                              onTap: () {},
+                              child: Card(
+                                  child: ListTile(
+                                title:
+                                    Text("${snapshot.data![index]['title']}"),
+                                trailing:
+                                    Text("${snapshot.data![index]['note']}"),
+                              ))),
+                        );
                       });
                 }
-                return Center(child: Text("No Note Added"));
+                return const Center(child: Text("No Note Added"));
               }),
 
           // Center(
@@ -65,7 +177,11 @@ class _HomeState extends State<Home> {
     );
   }
 }
-
+// Card(
+// child: ListTile(
+// title: Text("${snapshot.data![index]['title']}"),
+// trailing: Text("${snapshot.data![index]['note']}"),
+// ))
 // Column(
 // mainAxisAlignment: MainAxisAlignment.center,
 // children: [
